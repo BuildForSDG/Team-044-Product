85ef56db28d810182b22e3adf44eb2fe7c7af61c
Tosin Jimoh <jayoluwatosin@gmail.com>
2020-05-29 18:52:04 +0100

    Socket-io-integration

--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
-const app = async () => '#BuildforSDG';
+const express = require('express');
+const cors = require('cors');
+const socketIo = require('socket.io');
 
-export default app;
+const port = process.env.PORT || 4000;
+
+const dbConnect = require('./dbConnect');
+const initializeSocketIo = require('./socket');
+
+const app = express();
+
+const routes = require('./routes');
+
+// Database connection
+dbConnect();
+
+app.use(cors());
+
+// Routes
+app.use(routes);
+
+// Server Connection
+const server = app.listen(port, () => {
+  console.log('Server is listening');
+});
+
+const io = socketIo(server);
+
+initializeSocketIo(io);
